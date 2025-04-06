cda1ab493278130cc1290d4826e483b50f765885
Artem Golubev <artmglbv@gmail.com>
2025-04-06 22:32:08 +0300

    feat: delete them button, schedule page remove tailwind

--- src/app/schedule/days-list/day/day.component.ts
+++ src/app/schedule/days-list/day/day.component.ts
@@ -1,13 +1,13 @@
-import { Component, input } from '@angular/core';
-import { MatDividerModule } from '@angular/material/divider';
+import { Component, input } from "@angular/core";
+import { MatDividerModule } from "@angular/material/divider";
 
 @Component({
-  selector: 'app-day',
-  standalone: true,
-  imports: [MatDividerModule],
-  templateUrl: './day.component.html',
-  styleUrl: './day.component.scss'
+    selector: "app-day",
+    standalone: true,
+    imports: [MatDividerModule],
+    templateUrl: "./day.component.html",
+    styleUrl: "./day.component.scss",
 })
 export class DayComponent {
-  public day = input.required<any>();
+    public day = input.required<any>();
 }
